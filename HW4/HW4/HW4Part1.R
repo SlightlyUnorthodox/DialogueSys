6b07f378b824ed7a059fab1a2e8b579e2441b38a
Dax <gerts.dax1@gmail.com>
2016-09-24 13:50:53 -0400

    Added rough answers for part1 q1 b) and c).

--- HW4/HW4/HW4Part1.R
+++ HW4/HW4/HW4Part1.R
@@ -108,4 +108,14 @@ print(kappa)
 # Exact kappa estimate
 print(kappa$kappa)
 
-# 2. 
+# Q1. b) If you were planning to conduct research on these tagged data, would the kappa be sufficiently high for you to do so? Explain your reasoning. There is no absolute threshold that is “sufficiently high”, so you must contextualize your answer and give the reason(s) for it.
+
+print("Q1. b)
+Using the standards set by Landis and Koch, who stated that a kappa of 0.41 - 0.60 as 'moderate aggreement', it could be viewed that there is sufficient agreement to conduct research with these annotators. However this assessment is entirely contextual, with several other factors to consider. Primarily, it is known that kappa tends to score lower on sets with fewer codes, so it could be inferred that the kappa score is potentially underrating the aggreement between annotators. Further, the kappa was calculated on a confidence interval with alpha = 0.05, resulting in an interval of 0.29 to 0.71, the possible aggreement could be anywhere from 'slight' to 'substantial' . While a definitive answer cannot be provided, these results should be enough to proceed with research as long as the issues inherent in the problem are clearly addressed. ")
+
+# Q1. c)
+
+print("Q1. c) 
+    We compute kappa in order to validate the use multiple annotators to discuss the same problems. If they have no aggreement, no inferences could be made from the combination or interaction of their results.
+    
+    Consequently, we use multiple annotators (rather than a single annotator) in order to account for the fact that no single annotator will ever give a definitive 'true' rendering of the data. We hope that by using multiple annotators, each with different methodologies for identifying codes, there will be sufficient cross-pollination to account for the potential codes that would be missed or by any single annotator.")
